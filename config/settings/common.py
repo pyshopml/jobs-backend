82802bc64d6a6f2272ff5fa937020b2d68c3b018
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-07 19:19:31 +1000

    Add custom user model with email as login

--- config/settings/common.py
+++ config/settings/common.py
@@ -30,7 +30,7 @@ THIRD_PARTY_APPS = (
     'rest_framework',
 )
 LOCAL_APPS = (
-    # Your stuff: custom apps go here
+    'jobs_backend.users',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -199,8 +199,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # Custom user app defaults
-# todo: change AUTH_USER_MODEL after adding custom model
-# AUTH_USER_MODEL = ''
+AUTH_USER_MODEL = 'users.User'
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
 ADMIN_URL = r'^admin/'
