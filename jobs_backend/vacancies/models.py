7ab6468880ebf4901ee337fa1bf0158fde44f499
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-13 21:13:16 +1000

    jobs-010: Serializer, viewset, urls

--- jobs_backend/vacancies/models.py
+++ jobs_backend/vacancies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=1000)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
