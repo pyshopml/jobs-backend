e5a74b64df7fd143230594366b550ba4b2ac8e9a
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-18 16:43:19 +1000

    jobs-012: All endpoints to browsable API

--- config/urls.py
+++ config/urls.py
@@ -3,23 +3,26 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from jobs_backend.users.urls import users, account
+from jobs_backend.views import APIRoot
 
-# All api endpoints should be included here
+
 api_urlpatterns = [
-    url(r'^', include(users)),
-    url(r'^account/', include(account)),
+    # All api endpoints should be included here
+    url(r'^users/',
+        include('jobs_backend.users.urls.users', namespace='users')),
+    url(r'^account/',
+        include('jobs_backend.users.urls.account', namespace='account')),
 ]
 
 urlpatterns = [
+    *api_urlpatterns,
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
-
-    url(r'^api/', include(api_urlpatterns)),
-
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
-
+]
+urlpatterns += [
+    url(r'^$', APIRoot.as_view(urlpatterns=urlpatterns, app_namespace='api_v1'), name='api_root')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
