ff83f95f0f0847ae9fe494bc53efadb0847efc14
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-18 16:52:10 +1000

    jobs-010: changed urlpatterns for browsable APIRoot compatibility

--- jobs_backend/vacancies/urls.py
+++ jobs_backend/vacancies/urls.py
@@ -1,13 +1,9 @@
-from django.conf.urls import include, url
-
 from rest_framework.routers import DefaultRouter
 
 from .views import VacancyViewSet
 
 
 vacancy_router = DefaultRouter()
-vacancy_router.register(r'vacancy', VacancyViewSet)
+vacancy_router.register(r'', VacancyViewSet)
 
-urlpatterns = [
-    url(r'^', include(vacancy_router.urls)),
-]
+urlpatterns = vacancy_router.urls
