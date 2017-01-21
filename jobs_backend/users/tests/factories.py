8ac79c2a9b1e9eafc51e79c6978948cbd84e1666
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-21 19:07:21 +1000

    jobs-012: User factories

---
+++ jobs_backend/users/tests/factories.py
@@ -0,0 +1,21 @@
+import factory
+
+from ..models import User
+
+
+class BaseUserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    name = factory.Sequence(lambda n: 'name%s' % (n+1))
+    email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.name)
+
+
+class ActiveUserFactory(BaseUserFactory):
+    is_active = True
+
+
+class AdminFactory(BaseUserFactory):
+    is_superuser = True
+    is_staff = True
+    is_active = True
