272b2b2c1f263f0c929b9382002226479bd6e4d4
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-22 23:43:07 +1000

    jobs-010: Basic model tests

--- jobs_backend/vacancies/models.py
+++ jobs_backend/vacancies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Vacancy(models.Model):
@@ -9,3 +10,6 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('vacancies:vacancy-detail', kwargs={'pk': self.pk})
