dbb0d2001ed6650c77be4600a008fcb83c54e106
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-23 19:40:50 +1000

    jobs-010: Switched to HyperlinkedModelSerializer

--- jobs_backend/vacancies/serializers.py
+++ jobs_backend/vacancies/serializers.py
@@ -3,10 +3,15 @@ from rest_framework import serializers
 from .models import Vacancy
 
 
-class VacancySerializer(serializers.ModelSerializer):
+class VacancySerializer(serializers.HyperlinkedModelSerializer):
     """
     Common vacancy model serializer
     """
     class Meta:
         model = Vacancy
-        fields = ('id', 'title', 'description', 'created_on', 'modified_on')
+        fields = (
+            'id', 'url', 'title', 'description', 'created_on', 'modified_on'
+        )
+        extra_kwargs = {
+            'url': {'view_name': 'vacancies:vacancy-detail', 'read_only': True}
+        }
