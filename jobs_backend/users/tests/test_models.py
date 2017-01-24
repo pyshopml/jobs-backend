1e6eff1ec8e58fcfc49bea35d712126f8fbe1043
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-24 19:10:07 +1000

    jobs-012: Tests for UserManager's common creation function

--- jobs_backend/users/tests/test_models.py
+++ jobs_backend/users/tests/test_models.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.core import mail
 from django.test import TestCase
 
+from ..models import User
 from . import factories
 
 
 class UserTestCase(TestCase):
+
     def setUp(self):
         self.user_obj = factories.BaseUserFactory.build()
 
@@ -40,4 +42,33 @@ class UserTestCase(TestCase):
 
 
 class UserManagerTestCase(TestCase):
-    pass
+
+    def setUp(self):
+        self.data = {
+            'email': 'obi-wan.kenobi@example.com',
+            'name': 'Obi-Wan Kenobi',
+            'password': "These aren't the Droids your looking for",
+        }
+
+    def test_ok_common_create_user(self):
+        user = User.objects._create_user(**self.data)
+
+        self.assertTrue(user.pk)
+        self.assertEqual(user.email, self.data['email'])
+        self.assertEqual(user.name, self.data['name'])
+
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+        self.assertTrue(user.has_usable_password())
+        self.assertTrue(user.check_password(self.data['password']))
+
+    def test_fail_common_create_user_with_empty_email(self):
+        self.data['email'] = ''
+        self.assertRaises(ValueError, User.objects._create_user, **self.data)
+
+    def test_ok_common_create_user_normalize_email(self):
+        self.data['email'] = 'obi-wan.kenobi@ExAmPlE.cOm'
+        user = User.objects._create_user(**self.data)
+        self.assertEqual(user.email, 'obi-wan.kenobi@example.com')
