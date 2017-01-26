72769e74accf708878c8b4dae06f468ff287c536
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2017-01-26 20:05:13 +1000

    jobs-012: Tests for UserEmailBaseTest.__init__

--- jobs_backend/users/tests/test_utils.py
+++ jobs_backend/users/tests/test_utils.py
@@ -1,9 +1,12 @@
-import unittest
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.test import TestCase, RequestFactory, override_settings
 
+from . import factories
 from .. import utils
 
 
-class UIDTest(unittest.TestCase):
+class UIDTest(TestCase):
 
     def test_ok_encode_uid(self):
         self.assertEqual(utils.encode_uid(1), 'MQ')
@@ -17,4 +20,42 @@ class UIDTest(unittest.TestCase):
         self.assertEqual(utils.decode_uid('OTk5'), '999')
         self.assertEqual(
             utils.decode_uid('dXNlckBleGFtcGxlLmNvbQ'), 'user@example.com'
+        )
+
+
+class UserEmailBaseTest(TestCase):
+
+    def setUp(self):
+        self.rf = RequestFactory()
+        self.req = self.rf.get('/')
+        self.user = factories.ActiveUserFactory.create()
+        # self.req.user = self.user
+
+    def test_ok_init(self):
+        email = utils.UserEmailBase(self.req, self.user)
+
+        self.assertEqual(email.from_email, settings.DEFAULT_FROM_EMAIL)
+        self.assertEqual(email.user.email, self.user.email)
+        self.assertEqual(email.protocol, self.req.scheme)
+        self.assertEqual(str(email.site), 'testserver')
+
+    def test_ok_init_from_email(self):
+        from_ = 'noreply <noreply@example.com>'
+        email = utils.UserEmailBase(self.req, self.user, from_email=from_)
+        self.assertEqual(email.from_email, from_)
+
+    def test_ok_get_context(self):
+        email = utils.UserEmailBase(self.req, self.user)
+        pregenerated_token = default_token_generator.make_token(email.user)
+
+        self.assertDictEqual(
+            email.get_context(),
+            {
+                'user': self.user,
+                'protocol': self.req.scheme,
+                'domain': 'testserver',
+                'site': email.site,
+                'uid': 'MQ',
+                'token': pregenerated_token,
+            }
         )
