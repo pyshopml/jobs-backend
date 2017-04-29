532b0f9388e19573edf4a0309050f2915fcb6290
Fukalov Dmitri <2mitrij@gmail.com>
2017-04-29 18:23:44 +0300

    jobs-096: Extension of the vacancy model

--- jobs_backend/vacancies/admin.py
+++ jobs_backend/vacancies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Category, Vacancy
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    pass
--- jobs_backend/vacancies/models.py
+++ jobs_backend/vacancies/models.py
@@ -2,9 +2,32 @@ from django.db import models
 from django.urls import reverse
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    parent = models.ForeignKey('self', null=True, blank=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
 class Vacancy(models.Model):
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=1000)
+    salary_min = models.PositiveIntegerField(null=True, blank=True)
+    salary_max = models.PositiveIntegerField(null=True, blank=True)
+    keywords = models.ManyToManyField(Tag)
+    busyness = models.PositiveSmallIntegerField(null=True, blank=True)
+    remote_work = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, null=True, blank=True,
+                                 on_delete=models.PROTECT)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
